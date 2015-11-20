2b5b80bd97ecee0158db2538a3e569b5bbd2096e
KathaK <ka.kuderer@gmail.com>
2015-11-20 17:00:40 +0000

    Added errorhandler and appconfig

--- keyserver.py
+++ keyserver.py
@@ -23,10 +23,11 @@ def login():
         if validUser:
             session["logged_in"] = True
             flash("Welcome " + username + " to NapierKeyServer")
-            return redirect(url_for(""))
+            return redirect(url_for("")) # TODO
         else:
             error = "Invalid user credentials!"
     else:
+	app.logger.error("Login request is not POST method")
         error = "Invalid request."
     
     return render_template("login.html", error=error)
@@ -37,3 +38,10 @@ def logout():
     session.pop("logged_in", None)
     flash("You are now logged out")
     return redirect(url_for("login"))
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return "Sorry :( This page does not exist. 404"
+
+if __name__ == "__main__":
+    app.run(app.config["HOST"])
