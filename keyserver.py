bba9d18087666111cbbc354f96a8f4c4e3040aaf
KathaK <ka.kuderer@gmail.com>
2015-11-25 17:31:02 +0000

    Implemented methods for database creating, opening and querying

--- keyserver.py
+++ keyserver.py
@@ -1,12 +1,69 @@
-from flask import Flask
+from flask import Flask, g, render_template
+import sqlite3
 
 
 app = Flask(__name__)
 app.config.from_envvar("NAPIER_KEYSERVER_CONFIG")
 
+################################################################
+# database 
+
+def connect_db():
+    return sqlite3.connect(app.config["DATABASE"])
+
+def get_db():
+    db = getattr(g, "_database", None)
+    if db is None:
+        db = g._database = connect_db()
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, "_database", None)
+    if db is not None:
+        db.close()
+
+def query_db(query, args=(), one=False):
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
+
+def fill_db():
+    print("Filling database with examples")
+    with app.app_context():
+        db = get_db()
+        file = open("exampledata/users.example", "r")
+        for line in file:
+            line = line.rstrip("\n")
+            print(line.split("-"))
+            values = line.split("-")
+            q = "INSERT INTO users (username, pwdhash, realname, privkeyenc, pubkey) VALUES (?,?,?,?,?)"
+            query_db(q, values)
+        db.commit()
+        file = open("exampledata/friends.example", "r")
+        for line in file:
+            line = line.rstrip("\n")
+            print(line.split("-"))
+            values = line.split("-")
+            q = "INSERT INTO friends (username, friendlist) VALUES (?,?)"
+            query_db(q, values)
+        db.commit()
+
+def init_db():
+    print("Initializing database.")
+    with app.app_context():
+        db = get_db()
+        with app.open_resource("schema.sql", mode="r") as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+        fill_db()
+#
+################################################################
+
 @app.route("/")
 def mainpage():
-    return render_template("mainpage.html")
+    return "mainpage"#render_template("mainpage.html")
 
 
 @app.route("/register", methods=["GET", "POST"])
@@ -45,4 +102,5 @@ def page_not_found(error):
     return "Sorry :( This page does not exist. 404"
 
 if __name__ == "__main__":
-    app.run(app.config["HOST"])
+    app.logger.debug("Running keyserver on http://" + app.config["HOST"] + ":" + str(app.config["PORT"]))
+    app.run(app.config["HOST"], app.config["PORT"])
